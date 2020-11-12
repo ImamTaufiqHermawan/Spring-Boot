3a2ee402ecc7a767a248656d25d254e96f7a7c94
Imam Taufiq Hermawan <Imamtaufiq133@gmail.com>
2020-11-12 19:52:49 +0700

    first Commit

---
+++ src/main/java/com/example/crud_with_springboot/CrudWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud_with_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWithSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudWithSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crud_with_springboot/CrudWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_with_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWithSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
