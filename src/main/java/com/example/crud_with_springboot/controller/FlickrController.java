d47c3279e32cea3e65a5ca959db8549ac89e02fd
Imam Taufiq Hermawan <Imamtaufiq133@gmail.com>
2020-11-13 20:51:56 +0700

    add flickr

---
+++ src/main/java/com/example/crud_with_springboot/controller/FlickrController.java
@@ -0,0 +1,53 @@
+package com.example.crud_with_springboot.controller;
+
+import com.flickr4java.flickr.Flickr;
+import com.flickr4java.flickr.FlickrException;
+import com.flickr4java.flickr.REST;
+import com.flickr4java.flickr.photos.Photo;
+import com.flickr4java.flickr.photos.PhotoList;
+import com.flickr4java.flickr.photos.PhotosInterface;
+import com.flickr4java.flickr.photos.SearchParameters;
+import com.flickr4java.flickr.test.TestInterface;
+import java.util.Collection;
+import java.util.Collections;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.springframework.beans.factory.annotation.Value;
+
+@RestController
+public class FlickrController {
+    
+    @Value("${api.key}")
+    private String apiKey;
+    
+    @Value("${secret.key}")
+    private String secretKey;
+    
+    @GetMapping("/flickr")
+    public PhotoList<Photo> search(@RequestParam String text) throws FlickrException {
+        Flickr f = new Flickr(apiKey, secretKey, new REST());
+        // TestInterface testInterface = f.getTestInterface();
+        // Collection results = testInterface.echo(Collections.EMPTY_MAP);
+        
+        PhotosInterface photos = f.getPhotosInterface();
+        SearchParameters params = new SearchParameters();
+        params.setMedia("videos"); // One of "photos", "videos" or "all"
+        params.setExtras(Stream.of("media").collect(Collectors.toSet()));
+        params.setText(text);
+        PhotoList<Photo> results = photos.search(params, 5, 0);
+
+//        results.forEach(p ->
+//        {
+//            System.out.println(String.format("Title: %s", p.getTitle()));
+//            System.out.println(String.format("Media: %s", p.getMedia()));
+//            System.out.println(String.format("Original Video URL: %s", p.getVideoOriginalUrl()));
+//        });
+        return results;
+    }
+    
+
+}
--- src/main/java/com/example/crud_with_springboot/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.crud_with_springboot.repository;
-
-import com.example.crud_with_springboot.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-    Product findByName(String name);
-}
